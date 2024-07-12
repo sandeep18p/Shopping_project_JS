dfdb2912983aa91867d8a8a208a5c0e1ec0dad92
sandeep18p <sandeep.pansari98@gmail.com>
2024-07-12 14:37:08 +0530

    0ne-3

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 // myProducts.filter((item)=>item.title.includes(search.value))
 
 // myCartProductArray = myProducts.filter((item)=> myCartIDs.includes(item.id))
+
+function singup(){
+
+//   e.preventDefault();
+  console.log('woktin')
+  window.location.href="singup/index.html";
+}
+
+function singin(){
+
+    //   e.preventDefault();
+      console.log('woktin')
+      window.location.href="login/index.html";
+    }
